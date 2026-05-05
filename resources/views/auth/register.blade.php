2f1a21c7d200005ca85be2a6462015b29228c766
dieger75 <dieger75@hotmail.com>
2026-05-06 00:46:23 +0200

    seccion 30-177

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -44,9 +44,9 @@
                 {{ __('Already registered?') }}
             </a>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
         </div>
+        <x-primary-button class="ms-4">
+            {{ __('Register') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
