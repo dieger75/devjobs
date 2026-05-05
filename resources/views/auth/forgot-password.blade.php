53423bade513e8fc6ddf608b8185ea5e5c203bd2
dieger75 <dieger75@hotmail.com>
2026-05-06 00:58:52 +0200

    seccion 30-180

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('¿Has olvidado tu password? No te preocupes. Solo tienes que indicarnos tu email y te enviaremos un enlace para restablecerla, con el que podrás elegir una nueva.') }}
     </div>
 
     <!-- Session Status -->
@@ -16,10 +16,22 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <div class="flex items-center justify-between my-5">
+            <x-link
+                :href="route('login')"
+            >
+                Iniciar Sesión
+            </x-link>
+
+            <x-link
+                :href="route('register')"
+            >
+                Crear Cuenta
+            </x-link>           
         </div>
+
+        <x-primary-button class="w-full justify-center">
+            {{ __('Enviar instrucciones') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+<label {{ $attributes->merge(['class' => 'block font-bold text-sm text-gray-500 uppercase mb-2']) }}>
     {{ $value ?? $slot }}
 </label>
